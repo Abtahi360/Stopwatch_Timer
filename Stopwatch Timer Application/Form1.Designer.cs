18743814b3c84f90b418db57e5760fc298446cc9
ABTAHI ISLAM <23-50434-1@student.aiub.edu>
2025-03-10 00:55:25 +0600

    first commit

---
+++ Stopwatch Timer Application/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿using System.Windows.Forms;
+
+namespace Stopwatch_Timer_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Reset = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Pause = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(113)))), ((int)(((byte)(133)))));
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1584, 861);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.listBox1);
+            this.panel2.Controls.Add(this.Reset);
+            this.panel2.Controls.Add(this.Stop);
+            this.panel2.Controls.Add(this.Pause);
+            this.panel2.Controls.Add(this.Start);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(684, 308);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(543, 396);
+            this.panel2.TabIndex = 0;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(269, 282);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(54, 51);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(421, 312);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 48);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(421, 237);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 48);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(117, 218);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(177, 39);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Lab Timer";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.listBox1.Font = new System.Drawing.Font("Segoe Fluent Icons", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 32;
+            this.listBox1.Location = new System.Drawing.Point(44, 260);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(322, 100);
+            this.listBox1.TabIndex = 7;
+            // 
+            // Reset
+            // 
+            this.Reset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(80)))), ((int)(((byte)(146)))));
+            this.Reset.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Reset.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Reset.Location = new System.Drawing.Point(421, 159);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(93, 42);
+            this.Reset.TabIndex = 6;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = false;
+            this.Reset.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(80)))), ((int)(((byte)(146)))));
+            this.Stop.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Stop.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Stop.Location = new System.Drawing.Point(175, 159);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(93, 42);
+            this.Stop.TabIndex = 5;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = false;
+            this.Stop.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Pause
+            // 
+            this.Pause.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(80)))), ((int)(((byte)(146)))));
+            this.Pause.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pause.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Pause.Location = new System.Drawing.Point(299, 159);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(93, 42);
+            this.Pause.TabIndex = 4;
+            this.Pause.Text = "Pause";
+            this.Pause.UseVisualStyleBackColor = false;
+            this.Pause.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(80)))), ((int)(((byte)(146)))));
+            this.Start.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start.ForeColor = System.Drawing.Color.White;
+            this.Start.Location = new System.Drawing.Point(44, 159);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(93, 42);
+            this.Start.TabIndex = 3;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Swis721 BlkOul BT", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(63, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(428, 57);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "00 : 00 : 00 : 00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(133, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Stopwatch Timer";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Stopwatch Timer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button Pause;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Stopwatch Timer Application/Form1.cs
@@ -0,0 +1,166 @@
+﻿using Stopwatch_Timer_Application.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stopwatch_Timer_Application
+{
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer timer;
+        int hour, minute, second, millisecond;
+        List<string> historyList = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer = new System.Timers.Timer();
+            timer.Interval = 10;
+            timer.Elapsed += Timer_Elapsed;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            hour = 0;
+            minute = 0;
+            second = 0;
+            millisecond = 0;
+            label2.Text = "00 : 00 : 00 : 00";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string currentTime = label2.Text;
+            historyList.Add(currentTime); 
+            UpdateHistoryDisplay();
+
+            timer.Stop();
+            hour = 0;
+            minute = 0;
+            second = 0;
+            millisecond = 0;
+            label2.Text = "00 : 00 : 00 : 00";
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private bool isDarkTheme = false;
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (isDarkTheme)
+            {
+                this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(113)))), ((int)(((byte)(133)))));
+                this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+                this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+                this.label1.ForeColor = System.Drawing.Color.Black;
+                this.label3.ForeColor = System.Drawing.Color.Black;
+                this.label2.ForeColor = System.Drawing.Color.Black;
+                this.listBox1.ForeColor = System.Drawing.Color.Black;
+                this.Start.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(80)))), ((int)(((byte)(146)))));
+                this.Stop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(80)))), ((int)(((byte)(146)))));
+                this.Reset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(80)))), ((int)(((byte)(146)))));
+                this.Pause.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(80)))), ((int)(((byte)(146)))));
+            }
+            else
+            {
+                this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+                this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+                this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+                this.label1.ForeColor = System.Drawing.Color.White;
+                this.label3.ForeColor = System.Drawing.Color.White;
+                this.label2.ForeColor = System.Drawing.Color.White;
+                this.listBox1.ForeColor = System.Drawing.Color.White;
+                this.Start.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(145)))), ((int)(((byte)(255)))));
+                this.Stop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(145)))), ((int)(((byte)(255)))));
+                this.Reset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(145)))), ((int)(((byte)(255)))));
+                this.Pause.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(145)))), ((int)(((byte)(255)))));
+            }
+
+            isDarkTheme = !isDarkTheme;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            historyList.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                millisecond++;
+                if (millisecond == 100)
+                {
+                    millisecond = 0;
+                    second++;
+                }
+                if (second == 60)
+                {
+                    second = 0;
+                    minute++;
+                }
+                if (minute == 60)
+                {
+                    minute = 0;
+                    hour++;
+                }
+                label2.Text = string.Format("{0} : {1} : {2} : {3}",
+                    hour.ToString().PadLeft(2, '0'),
+                    minute.ToString().PadLeft(2, '0'),
+                    second.ToString().PadLeft(2, '0'),
+                    millisecond.ToString().PadLeft(2, '0'));
+            }));
+        }
+
+        private void UpdateHistoryDisplay()
+        {
+            listBox1.Items.Clear();
+            foreach (string entry in historyList)
+                listBox1.Items.Add(entry);
+        }
+    }
+}
